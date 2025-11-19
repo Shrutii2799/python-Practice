7b8271d30e09caac90fc9c20e7e38178bf2741f2
Shrutii2799 <shrutimutyal@gmail.com>
2025-11-19 23:44:10 +0530

    progress till day 9

---
+++ Day 9/Blind Auction Project/main.py
@@ -0,0 +1,8 @@
+# TODO-1: Ask the user for input
+input("")
+# TODO-2: Save data into dictionary {name: price}
+
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
+
+
